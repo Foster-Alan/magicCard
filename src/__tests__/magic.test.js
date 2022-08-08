3d75e99a07656bb5d1051c37f1380c1d106dc850
Alan Foster <alan-af@outlook.com>
2022-08-08 19:31:58 -0300

    exercicio 1

--- src/__tests__/magic.test.js
+++ src/__tests__/magic.test.js
@@ -1,21 +1,24 @@
-// const { getMagicCard } = require('../magic');
-// require('../../startest/mockSetup');
+const { getMagicCard } = require('../magic');
+require('../../startest/mockSetup');
 
 jest.setTimeout(10000);
 
 describe('1. Testes da função getMagicCard', () => {
   it('1.1 Verifique se getMagicCard é uma função.', () => {
     // implemente seus testes aqui
+    expect(typeof getMagicCard).toBe('function');
   });
 
   it('1.2 Verifique se a função fetch foi chamada.', async () => {
-    // await getMagicCard('130550');
+    await getMagicCard('130550');
     // implemente seus testes aqui
+    expect(fetch).toHaveBeenCalled();
   });
 
   it('1.3 Verifique se a função fetch foi chamada com o endpoint correto', async () => {
-    // await getMagicCard('130550');
+    await getMagicCard('130550');
     // implemente seus testes aqui
+    expect(fetch).toHaveBeenCalledWith('https://api.magicthegathering.io/v1/cards/130550');
   });
 });
 
